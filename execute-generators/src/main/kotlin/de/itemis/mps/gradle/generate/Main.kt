fda2f3b5251f0a8b5e644023c351346aa616c520
Sergej Koščejev <sergej@koscejev.cz>
2022-02-28 15:47:58 +0100

    Make it possible to show usage without log4j and MPS
    Rearrange code so that it's possible to show usage information without
    log4j or MPS on the classpath.

--- execute-generators/src/main/kotlin/de/itemis/mps/gradle/generate/Main.kt
+++ execute-generators/src/main/kotlin/de/itemis/mps/gradle/generate/Main.kt
@@ -7,18 +7,17 @@ import de.itemis.mps.gradle.project.loader.Args
 import de.itemis.mps.gradle.project.loader.executeWithProject
 import org.apache.log4j.Logger
 
-private val logger = Logger.getLogger("de.itemis.mps.gradle.generate")
-
 class GenerateArgs(parser: ArgParser) : Args(parser) {
     val models by parser.adding("--model",
             help = "list of models to generate")
 }
 
 
-fun main(args: Array<String>) = mainBody {
-
+fun main(args: Array<String>) = mainBody("execute-generators") {
     val parsed = ArgParser(args).parseInto(::GenerateArgs)
     var result = false
+
+    val logger = Logger.getLogger("de.itemis.mps.gradle.generate")
 
     try {
         result = executeWithProject(parsed) { project -> generateProject(parsed, project) }
