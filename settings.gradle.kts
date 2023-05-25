079c928722f35ba0a1607fc3fc497a0947295015
Sergej Koščejev <sergej@koscejev.cz>
2023-05-25 11:43:07 +0200

    Create version catalog to unify MPS dependency

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,14 @@
 rootProject.name = "mps-build-backends"
+
 include("execute-generators")
 include("project-loader")
 include("modelcheck")
 include("integration-tests")
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            library("mps", "com.jetbrains:mps:2021.2.5")
+        }
+    }
+}
