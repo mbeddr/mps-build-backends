a404b6c5b09419c3c56f2a7ca5243f756465a393
Sergej Koščejev <sergej@koscejev.cz>
2023-06-06 19:02:16 +0200

    build.gradle.kts: remove unused variable

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,6 @@ plugins {
 val kotlinVersion by extra { "1.6.10" }
 
 val kotlinArgParserVersion by extra { "2.0.7" }
-val mpsVersion by extra { "2021.2.5" }
 
 //this version needs to align with the version shiped with MPS found in the /lib folder otherwise, runtime problems will
 //surface because mismatching jars on the classpath.
