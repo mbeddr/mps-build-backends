a33526d1043c843dd02abda63ad2c3efe4f137ea
Sergej Koščejev <sergej@koscejev.cz>
2023-07-25 22:04:46 +0200

    Minor improvements

--- integration-tests/build.gradle.kts
+++ integration-tests/build.gradle.kts
@@ -22,19 +22,6 @@ dependencies {
 
 val SUPPORTED_MPS_VERSIONS = arrayOf("2021.1.4", "2021.2.6", "2021.3.2", "2022.2", "2022.3")
 
-fun toolchainForMpsVersion(mpsVersion: String): Action<JavaToolchainSpec> =
-    if (mpsVersion < "2022.3") {
-        Action<JavaToolchainSpec> {
-            vendor.set(JvmVendorSpec.matching("JetBrains"))
-            languageVersion.set(JavaLanguageVersion.of(11))
-        }
-    } else {
-        Action<JavaToolchainSpec> {
-            vendor.set(JvmVendorSpec.matching("JetBrains"))
-            languageVersion.set(JavaLanguageVersion.of(17))
-        }
-    }
-
 val GENERATION_TESTS = listOf(
     GenerationTest("generateBuildSolution", "generate-build-solution", listOf("--model", "my.build.script"),
         expectation = GenerationTestExpectation.SuccessWithFiles("solutions/my.build/build.xml")),
@@ -165,54 +152,61 @@ data class ModelCheckTest(val name: String, val project: String, val args: List<
 }
 
 fun configureJavaForMpsVersion(javaExec: JavaExec, mpsHome: File, mpsVersion: String) {
-    val toolchainAction = toolchainForMpsVersion(mpsVersion)
-    val launcher = serviceOf<JavaToolchainService>().launcherFor(toolchainAction)
+    val launcher = serviceOf<JavaToolchainService>().launcherFor {
+        vendor.set(JvmVendorSpec.matching("JetBrains"))
+        languageVersion.set(JavaLanguageVersion.of(if (mpsVersion < "2022") 11 else 17))
+    }
     javaExec.javaLauncher.set(launcher)
 
-    javaExec.systemProperty("jna.boot.library.path", mpsHome.resolve("lib/jna/${System.getProperty("os.arch")}"))
+    if (mpsVersion >= "2022.3") {
+        javaExec.systemProperty("jna.boot.library.path", mpsHome.resolve("lib/jna/${System.getProperty("os.arch")}"))
+    }
 
-    val modules = listOf(
-        "java.base/java.io",
-        "java.base/java.lang",
-        "java.base/java.lang.reflect",
-        "java.base/java.net",
-        "java.base/java.nio",
-        "java.base/java.nio.charset",
-        "java.base/java.text",
-        "java.base/java.time",
-        "java.base/java.util",
-        "java.base/java.util.concurrent",
-        "java.base/java.util.concurrent.atomic",
-        "java.base/jdk.internal.vm",
-        "java.base/sun.nio.ch",
-        "java.base/sun.nio.fs",
-        "java.base/sun.security.ssl",
-        "java.base/sun.security.util",
-        "java.desktop/java.awt",
-        "java.desktop/java.awt.dnd.peer",
-        "java.desktop/java.awt.event",
-        "java.desktop/java.awt.image",
-        "java.desktop/java.awt.peer",
-        "java.desktop/javax.swing",
-        "java.desktop/javax.swing.plaf.basic",
-        "java.desktop/javax.swing.text.html",
-        "java.desktop/sun.awt.datatransfer",
-        "java.desktop/sun.awt.image",
-        "java.desktop/sun.awt",
-        "java.desktop/sun.font",
-        "java.desktop/sun.java2d",
-        "java.desktop/sun.swing",
-        "jdk.attach/sun.tools.attach",
-        "jdk.compiler/com.sun.tools.javac.api",
-        "jdk.internal.jvmstat/sun.jvmstat.monitor",
-        "jdk.jdi/com.sun.tools.jdi",
-        "java.desktop/sun.lwawt",
-        "java.desktop/sun.lwawt.macosx",
-        "java.desktop/com.apple.laf",
-        "java.desktop/com.apple.eawt",
-        "java.desktop/com.apple.eawt.event")
+    if (mpsVersion >= "2022") {
+        val modules = listOf(
+            "java.base/java.io",
+            "java.base/java.lang",
+            "java.base/java.lang.reflect",
+            "java.base/java.net",
+            "java.base/java.nio",
+            "java.base/java.nio.charset",
+            "java.base/java.text",
+            "java.base/java.time",
+            "java.base/java.util",
+            "java.base/java.util.concurrent",
+            "java.base/java.util.concurrent.atomic",
+            "java.base/jdk.internal.vm",
+            "java.base/sun.nio.ch",
+            "java.base/sun.nio.fs",
+            "java.base/sun.security.ssl",
+            "java.base/sun.security.util",
+            "java.desktop/java.awt",
+            "java.desktop/java.awt.dnd.peer",
+            "java.desktop/java.awt.event",
+            "java.desktop/java.awt.image",
+            "java.desktop/java.awt.peer",
+            "java.desktop/javax.swing",
+            "java.desktop/javax.swing.plaf.basic",
+            "java.desktop/javax.swing.text.html",
+            "java.desktop/sun.awt.datatransfer",
+            "java.desktop/sun.awt.image",
+            "java.desktop/sun.awt",
+            "java.desktop/sun.font",
+            "java.desktop/sun.java2d",
+            "java.desktop/sun.swing",
+            "jdk.attach/sun.tools.attach",
+            "jdk.compiler/com.sun.tools.javac.api",
+            "jdk.internal.jvmstat/sun.jvmstat.monitor",
+            "jdk.jdi/com.sun.tools.jdi",
+            "java.desktop/sun.lwawt",
+            "java.desktop/sun.lwawt.macosx",
+            "java.desktop/com.apple.laf",
+            "java.desktop/com.apple.eawt",
+            "java.desktop/com.apple.eawt.event"
+        )
 
-    javaExec.jvmArgs(modules.map { "--add-opens=$it=ALL-UNNAMED" })
+        javaExec.jvmArgs(modules.map { "--add-opens=$it=ALL-UNNAMED" })
+    }
 }
 
 /**
