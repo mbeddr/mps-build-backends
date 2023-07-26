ec97681b9891dd155f54f725268e1fb6659f4732
Sergej Koščejev <sergej@koscejev.cz>
2023-07-26 08:46:43 +0200

    project-loader: log formatting consistent with MPS
    Reuse the layout of the root logger where possible.

--- project-loader/src/main/kotlin/de/itemis/mps/gradle/logging/LoggingJul.kt
+++ project-loader/src/main/kotlin/de/itemis/mps/gradle/logging/LoggingJul.kt
@@ -2,9 +2,7 @@ package de.itemis.mps.gradle.logging
 
 import org.apache.commons.logging.Log
 import org.apache.commons.logging.impl.Jdk14Logger
-import java.util.logging.ConsoleHandler
-import java.util.logging.Level
-import java.util.logging.Logger
+import java.util.logging.*
 
 internal class LoggingJul : Logging {
     val logger = Logger.getLogger("de.itemis.mps")
@@ -17,6 +15,18 @@ internal class LoggingJul : Logging {
         if (logger.handlers.isEmpty()) {
             val handler = ConsoleHandler()
             handler.level = logger.level
+
+            // IDEA Environment uses a special IdeaLogRecordFormatter that provides timestamps and we'd rather reuse it
+            // to have consistent formatting and consistent timestamp reference point.
+            //
+            // Otherwise, use the default formatter.
+            val formatter = getRootLoggerConsoleFormatter()
+            if (formatter != null) {
+                handler.formatter = formatter
+
+                // Avoid duplicate log messages
+                logger.useParentHandlers = false
+            }
             logger.addHandler(handler)
         }
     }
@@ -29,4 +39,10 @@ internal class LoggingJul : Logging {
             LogLevel.ERROR -> Level.SEVERE
             LogLevel.OFF -> Level.OFF
         }
+
+    private fun getRootLoggerConsoleFormatter(): Formatter? =
+        Logger.getLogger("").handlers.asSequence()
+            .filterIsInstance(ConsoleHandler::class.java)
+            .map { it.formatter }
+            .singleOrNull()
 }
--- project-loader/src/main/kotlin/de/itemis/mps/gradle/logging/LoggingLog4j.kt
+++ project-loader/src/main/kotlin/de/itemis/mps/gradle/logging/LoggingLog4j.kt
@@ -2,10 +2,7 @@ package de.itemis.mps.gradle.logging
 
 import org.apache.commons.logging.Log
 import org.apache.commons.logging.impl.Log4JLogger
-import org.apache.log4j.ConsoleAppender
-import org.apache.log4j.Level
-import org.apache.log4j.Logger
-import org.apache.log4j.SimpleLayout
+import org.apache.log4j.*
 
 internal class LoggingLog4j : Logging {
 
@@ -22,18 +19,28 @@ internal class LoggingLog4j : Logging {
         val logger = Logger.getLogger("de.itemis.mps")
         logger.level = level.toLog4j()
 
-        if (level != LogLevel.OFF) {
-            val hasAppenderForSystemErr =
-                logger.allAppenders.asSequence().filterIsInstance<ConsoleAppender>().any { it.target == "System.err" }
+        val existingAppender = logger.allAppenders.asSequence().filterIsInstance<ConsoleAppender>()
+            .firstOrNull { it.target == "System.err" }
 
-            // Avoid adding the same appender twice
-            if (!hasAppenderForSystemErr) {
-                val appender = ConsoleAppender(SimpleLayout(), "System.err")
-                appender.threshold = level.toLog4j()
-                logger.addAppender(appender)
-            }
+        val layout = getRootLoggerConsoleLayout() ?: PatternLayout(PatternLayout.TTCC_CONVERSION_PATTERN)
+
+        val appender: ConsoleAppender
+
+        // Avoid adding the same appender twice
+        if (existingAppender != null) {
+            appender = existingAppender
+            appender.layout = layout
+        } else {
+            appender = ConsoleAppender(layout, "System.err")
+            logger.addAppender(appender)
         }
     }
+
+    private fun getRootLoggerConsoleLayout(): Layout? =
+        Logger.getRootLogger().allAppenders.asSequence()
+            .filterIsInstance(ConsoleAppender::class.java)
+            .map { it.layout }
+            .firstOrNull()
 
     override fun getLogger(name: String): Log = Log4JLogger(name)
 }
