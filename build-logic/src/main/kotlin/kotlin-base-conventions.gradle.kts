7596736eb19d5b7a8da6928e6459c9b580bd4170
Willem Jan Glerum <willem.jan.glerum@lunatech.nl>
2025-03-28 08:33:26 +0100

    Update to Kotlin 2.1.20 but keep 1.6.x compatibility (#33)
    See https://kotlinlang.org/docs/compatibility-modes.html
    
    Unfortunately we can't have the `allWarningsAsErrors` enabled as `KOTLIN_1_6` is deprecated, and we can't seem to suppress the deprecation warning when it's called in a task.

--- build-logic/src/main/kotlin/kotlin-base-conventions.gradle.kts
+++ build-logic/src/main/kotlin/kotlin-base-conventions.gradle.kts
@@ -1,4 +1,5 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 
 plugins {
     kotlin("jvm")
@@ -8,12 +9,14 @@ plugins {
 
 java {
     withSourcesJar()
+    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_11
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        jvmTarget = "11"
-        apiVersion = "1.6"
-        allWarningsAsErrors = true
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_11
+        apiVersion = KotlinVersion.KOTLIN_1_6
+        languageVersion = KotlinVersion.KOTLIN_1_6
     }
 }
